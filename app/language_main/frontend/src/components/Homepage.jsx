2da37572c42b03841f3a60a7b7e0face184eeb39
Anubhava2105 <118335179+Anubhava2105@users.noreply.github.com>
2024-07-10 16:12:18 +0530

    add new about us page along with team info

--- app/language_main/frontend/src/components/Homepage.jsx
+++ app/language_main/frontend/src/components/Homepage.jsx
@@ -1,48 +1,54 @@
-import React, { useRef, useEffect}  from 'react';
-import { Link } from 'react-router-dom';
-import './Homepage.css';
-import laptopImage from '../components/laptop.png';
-import SpanioLogo from '../components/spanio.jpg';
+import React, { useRef, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./Homepage.css";
+import Aboutus from "./Aboutus";
+import laptopImage from "../components/laptop.png";
+import SpanioLogo from "../components/spanio.jpg";
 // import pxfuel from '../../public/pxfuel.jpg';
-import image from '../components/img.jpg';
-import { useNavigate } from 'react-router-dom';
-import emailjs from '@emailjs/browser';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import image from "../components/img.jpg";
+import { useNavigate } from "react-router-dom";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Homepage = () => {  
-    const navigate = useNavigate();
+const Homepage = () => {
+  const navigate = useNavigate();
 
-    const handleSignIn = () => {
-      navigate('/login');
-    };
+  const handleSignIn = () => {
+    navigate("/login");
+  };
 
-    const handleSignUp = () =>{
-      navigate('/signup')
-    }
+  const handleSignUp = () => {
+    navigate("/signup");
+  };
 
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-  
+
     const formData = {
       firstname: form.current.firstname.value,
       lastname: form.current.lastname.value,
       user_email: form.current.user_email.value,
       message: form.current.message.value,
     };
-  
+
     emailjs
-      .send('service_6iw3a5k', 'template_fwwqned', formData, '-HQ_08N7f1ml_-oOK')
+      .send(
+        "service_6iw3a5k",
+        "template_fwwqned",
+        formData,
+        "-HQ_08N7f1ml_-oOK"
+      )
       .then(
         (result) => {
-          console.log('Message send');
-          toast.success('Message sent successfully', { autoClose: 700 });
+          console.log("Message send");
+          toast.success("Message sent successfully", { autoClose: 700 });
         },
         (error) => {
           console.log(error.text);
-          toast.error('Failed to send message', { autoClose: 700 }); 
+          toast.error("Failed to send message", { autoClose: 700 });
         }
       );
   };
@@ -74,20 +80,26 @@ const Homepage = () => {
           ipt.parentNode.classList.remove("focus");
         });
       });
-    }
-  }, []); 
+    };
+  }, []);
   return (
     <div className="wrapper">
       <header id="Home" className="header">
         <div className="header-left">
-          <img src={SpanioLogo} height="60px" width="80px" alt="Logo"/>
+          <img src={SpanioLogo} height="60px" width="80px" alt="Logo" />
           <h1>Spanio</h1>
         </div>
         <div className="header-right">
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#Contact">Contact</a></li>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <a onClick={() => navigate("/aboutus")}>About Us</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
           </ul>
         </div>
       </header>
@@ -97,10 +109,21 @@ const Homepage = () => {
 
         <div className="content">
           <div>
-            <p>Welcome to our Spanish Learning App where you can too master it like the natives do !</p>
-            <p>Sign up for Spanio+ today and embark on a learning adventure like never before. Learn new languages with assorted quiz and an associated learning material</p>
-            <button className="btn" onClick={handleSignIn}>SIGN IN</button>
-            <button className="btn" onClick={handleSignUp}>SIGN UP</button>
+            <p>
+              Welcome to our Spanish Learning App where you can too master it
+              like the natives do !
+            </p>
+            <p>
+              Sign up for Spanio+ today and embark on a learning adventure like
+              never before. Learn new languages with assorted quiz and an
+              associated learning material
+            </p>
+            <button className="btn" onClick={handleSignIn}>
+              SIGN IN
+            </button>
+            <button className="btn" onClick={handleSignUp}>
+              SIGN UP
+            </button>
           </div>
 
           <div className="laptop">
@@ -110,104 +133,155 @@ const Homepage = () => {
       </main>
 
       <section id="Contact" class="contact">
-            <div class="container">
-                <div class="left">
-                    <div class="form-wrapper">
-                        <div class="contact-heading">
-                            <h1>Let's work together<span>.</span></h1>
-                            <p class="text">Contact us: <a href="mailto:Spanio@mail
-                                .com">Spanio@mail.com</a></p>
-                        </div>
+        <div class="container">
+          <div class="left">
+            <div class="form-wrapper">
+              <div class="contact-heading">
+                <h1>
+                  Let's work together<span>.</span>
+                </h1>
+                <p class="text">
+                  Contact us:{" "}
+                  <a
+                    href="mailto:Spanio@mail
+                                .com"
+                  >
+                    Spanio@mail.com
+                  </a>
+                </p>
+              </div>
 
-                        <form  ref={form} class="contact-form" onSubmit={sendEmail}>
-                            <div class="input-wrap">
-                                <input class="contact-input" autocomplete="off" name="firstname"
-                                type="text" required/>
-                                <label>First Name</label>
-                                <i class="icon fa-solid fa-address-card"></i>
-                            </div>
-
-                            <div class="input-wrap">
-                                <input class="contact-input" autocomplete="off" name="lastname"
-                                type="text" required/>
-                                <label>Last Name</label>
-                                <i class="icon fa-solid fa-address-card"></i>
-                            </div>
-
-                            <div class="input-wrap w-100">
-                                <input class="contact-input" autocomplete="off" name="user_email"
-                                type="email" required/>
-                                <label>Email</label>
-                                <i class="icon fa-solid fa-envelope"></i>
-                            </div>
-
-                            <div class="input-wrap textarea w-100">
-                                <textarea name="message" autocomplete="off" 
-                                class="contact-input" required></textarea>
-                                <label>Message</label>
-                                <i class="icon fa-solid fa-inbox"></i>
-                            </div>
-
-                            <div class="contact-buttons">
-                                <button class="button upload">
-                                    <span>
-                                        <i class="fa-solid fa-paperclip"></i>
-                                        Add attachment
-                                    </span>
-                                    <input type="file" name="attachment"/>
-                                </button>
-                                <button class="button upload">
-                                <span>
-                                      <i class="fa-solid fa-paperclip"></i>
-                                        Send message
-                                    </span>
-                                  <input type="submit" value="Send message" class="button"/>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+              <form ref={form} class="contact-form" onSubmit={sendEmail}>
+                <div class="input-wrap">
+                  <input
+                    class="contact-input"
+                    autocomplete="off"
+                    name="firstname"
+                    type="text"
+                    required
+                  />
+                  <label>First Name</label>
+                  <i class="icon fa-solid fa-address-card"></i>
                 </div>
-                <div class="right">
-                    <div class="image-wrapper">
-                        <img src={image} class="img"/>
-                        <div class="wave-wrap">
-                            <svg class="wave" viewBox="0 0 783 1536" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path id="wave" d="M236.705 1356.18C200.542 1483.72 64.5004 1528.54 1 1535V1H770.538C793.858 63.1213 797.23 196.197 624.165 231.531C407.833 275.698 274.374 331.715 450.884 568.709C627.393 805.704 510.079 815.399 347.561 939.282C185.043 1063.17 281.908 1196.74 236.705 1356.18Z"/>
-                                </svg>                            
-                        </div>
-                        <svg class="dashed-wave" viewBox="0 0 345 877" fill="none" 
-                        xmlns="http://www.w3.org/2000/svg">
-                            <path id="dashed-wave" d="M0.5 876C25.6667 836.167 73.2 739.8 62 
-                            673C48 589.5 35.5 499.5 125.5 462C215.5 424.5 150 365 87 333.5C24 
-                            302 44 237.5 125.5 213.5C207 189.5 307 138.5 246 87C185 35.5 297 1 344.5 1"/>
-                        </svg>                            
-                    </div>
+
+                <div class="input-wrap">
+                  <input
+                    class="contact-input"
+                    autocomplete="off"
+                    name="lastname"
+                    type="text"
+                    required
+                  />
+                  <label>Last Name</label>
+                  <i class="icon fa-solid fa-address-card"></i>
                 </div>
+
+                <div class="input-wrap w-100">
+                  <input
+                    class="contact-input"
+                    autocomplete="off"
+                    name="user_email"
+                    type="email"
+                    required
+                  />
+                  <label>Email</label>
+                  <i class="icon fa-solid fa-envelope"></i>
+                </div>
+
+                <div class="input-wrap textarea w-100">
+                  <textarea
+                    name="message"
+                    autocomplete="off"
+                    class="contact-input"
+                    required
+                  ></textarea>
+                  <label>Message</label>
+                  <i class="icon fa-solid fa-inbox"></i>
+                </div>
+
+                <div class="contact-buttons">
+                  <button class="button upload">
+                    <span>
+                      <i class="fa-solid fa-paperclip"></i>
+                      Add attachment
+                    </span>
+                    <input type="file" name="attachment" />
+                  </button>
+                  <button class="button upload">
+                    <span>
+                      <i class="fa-solid fa-paperclip"></i>
+                      Send message
+                    </span>
+                    <input type="submit" value="Send message" class="button" />
+                  </button>
+                </div>
+              </form>
             </div>
-        </section>
+          </div>
+          <div class="right">
+            <div class="image-wrapper">
+              <img src={image} class="img" alt="upload" />
+              <div class="wave-wrap">
+                <svg
+                  class="wave"
+                  viewBox="0 0 783 1536"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="wave"
+                    d="M236.705 1356.18C200.542 1483.72 64.5004 1528.54 1 1535V1H770.538C793.858 63.1213 797.23 196.197 624.165 231.531C407.833 275.698 274.374 331.715 450.884 568.709C627.393 805.704 510.079 815.399 347.561 939.282C185.043 1063.17 281.908 1196.74 236.705 1356.18Z"
+                  />
+                </svg>
+              </div>
+              <svg
+                class="dashed-wave"
+                viewBox="0 0 345 877"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  id="dashed-wave"
+                  d="M0.5 876C25.6667 836.167 73.2 739.8 62 
+                            673C48 589.5 35.5 499.5 125.5 462C215.5 424.5 150 365 87 333.5C24 
+                            302 44 237.5 125.5 213.5C207 189.5 307 138.5 246 87C185 35.5 297 1 344.5 1"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* footer */}
       <div className="footer">
         <div class="uppercontent">
-          <div className="footerhead">
-            Spanio
-          </div>
+          <div className="footerhead">Spanio</div>
 
           <div class="leftportion">
             <div class="lpm">
               <p>Menu</p>
               <ul>
-                <li><a href="#Home">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#Contact">Contact</a></li>
+                <li>
+                  <a href="#Home">Home</a>
+                </li>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#Contact">Contact</a>
+                </li>
               </ul>
             </div>
-            
+
             <div class="lps">
               <p>Services</p>
               <ul>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Use</a></li>
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Terms of Use</a>
+                </li>
               </ul>
             </div>
           </div>
@@ -217,14 +291,13 @@ const Homepage = () => {
             <a href="mailto:Spanio@mail.com">Email: Spanio@mail.com</a>
           </div>
         </div>
-        
 
         <div class="lowercontent">
           Copyright © Spanio Technologies Pvt. Limited | All Right Reserved
-          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Homepage;
+export default Homepage;
